ab4888996102303a007445702da2dee6a5b560e9
ImtiazMahmod <imtiazmahmod2@gmail.com>
2021-11-20 22:41:57 +0600

    payment image hosting

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { initializeApp } = require("firebase-admin/app");
+const stripe = require("stripe")(process.env.STRIPE_SECRETE);
+const ObjectId = require("mongodb").ObjectId;
+const fileUpload = require("express-fileupload");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 ///firebase admin
 
@@ -51,12 +56,44 @@ async function run() {
     const appointmentCollection = database.collection("appointment");
 
     const usersCollection = database.collection("users");
+    const doctorsCollection = database.collection("doctors");
 
     ///appointment post
     app.post("/appointments", async (req, res) => {
       const appointment = req.body;
       const result = await appointmentCollection.insertOne(appointment);
       res.json(result);
+    });
+    ///doctors post
+    app.post("/doctors", async (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const pic = req.files.image;
+      const picData = pic.data;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+
+      const doctor = {
+        name,
+        email,
+        image: imageBuffer,
+      };
+
+      const result = await doctorsCollection.insertOne(doctor);
+      res.json(result);
+    });
+
+    ///load doctors
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    ///load specific load details
+    app.get("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await appointmentCollection.findOne({ _id: ObjectId(id) });
+      res.send(result);
     });
 
     ///load specific patient appointment
@@ -71,6 +108,20 @@ async function run() {
         // console.log(appointments);
         res.send(appointments);
       }
+    });
+    ///update payment appointmentInfo
+    app.put("/appointment/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          payment,
+        },
+      };
+      const result = await appointmentCollection.updateOne(filter, updateDoc);
+      console.log(result);
+      res.json(result);
     });
 
     //users info
@@ -124,6 +175,24 @@ async function run() {
       }
       res.send({ admin: isAdmin });
       console.log("isAdmin", isAdmin);
+    });
+
+    ///stripe payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const paymentInfo = req.body;
+      console.log(paymentInfo);
+      const amount = paymentInfo.price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+
+      res.json({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
   } finally {
   }
